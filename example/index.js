856069ebfe7dc875d648e8520e4628fd89777058
doowb <brian.woodward@gmail.com>
2017-02-01 20:14:22 -0500

    deps for examples

--- example/index.js
+++ example/index.js
@@ -12,19 +12,19 @@ var config = require('./config')(args);
 var app = new assemble();
 var sarge = new Sarge({indexer: args.i});
 
-search.indexer('lunr', require('./indexer-lunr')(config))
-search.indexer('algolia', require('./indexer-algolia')(config))
+sarge.indexer('lunr', require('./indexer-lunr')(config))
+sarge.indexer('algolia', require('./indexer-algolia')(config))
 
 app.src('docs/*.html', {cwd: __dirname})
-  .pipe(search.collect())
+  .pipe(sarge.collect())
   .on('data', function(){})
   .on('end', function() {
-    search.index(function(err) {
+    sarge.index(function(err) {
       if (err) {
         console.error(err);
         process.exit(1);
       }
-      console.log(search.files);
+      console.log(sarge.files);
       console.log('done');
       process.exit();
     });
